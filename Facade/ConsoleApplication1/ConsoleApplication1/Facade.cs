43e7f880a11d622e4e8f09dbfefe5e2af0acf62e
alexklimov <klimwork93@mail.ru>
2016-04-30 00:25:17 +0300

    add pattern Facade

---
+++ Facade/ConsoleApplication1/ConsoleApplication1/Facade.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace ConsoleApplication1
+{
+   public static  class Facade
+    {
+       static Subsystems.SubsystemA a = new Subsystems.SubsystemA();
+       static Subsystems.SubsystemB b = new Subsystems.SubsystemB();
+       static Subsystems.SubsystemC c = new Subsystems.SubsystemC();
+
+        public static void Operation1()
+        {
+            Console.WriteLine("Операция 1\n" +
+            a.A1() +
+            a.A2() +
+            b.B1());
+        }
+        public static void Operation2()
+        {
+            Console.WriteLine("Операция 2\n" +
+            b.B1() +
+            c.C1());
+        }
+    }
+}
---
+++ Facade/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //клиентский код
+            Facade.Operation1();
+            Facade.Operation2();
+
+            // Wait for user
+            Console.Read();
+
+        }
+    }
+}
